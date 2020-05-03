3ac0f7943f025c3d128bc1bd4f3ac78d00898c2f
sumukshashidhar <sumukuuu@gmail.com>
2020-05-03 22:42:45 +0530

    added documentation

--- docs/index.md
+++ docs/index.md
@@ -6,4 +6,6 @@ This is a simple python package to retrieve and plot live stock data
 
 # How to use?
 
-Run `run.py` and follow the instructions
+From the source folder, open up the yahoo_finance_fetch.py and pass the ticker as an argument. For example
+
+`python3 src/retrieve/yahoo_finance_fetch.py -t "AAPL" `
--- src/retrieve/yahoo_finance_fetch.py
+++ src/retrieve/yahoo_finance_fetch.py
@@ -8,30 +8,24 @@ import urllib.request
 import json
 from datetime import date
 import time
-
-
+import argparse
 
 # ticker = input('Enter the symbol for the stock you want to track. A full list can be found on the yahoo finance website \n \n')
 # ticker_get() #disable this if you pre specify the ticker directly in the file
-
-def set_ticker_and_url(ticker):
-    ticker = ticker
-    url = "https://query1.finance.yahoo.com/v7/finance/quote?symbols="+ ticker + "&range=1d&interval=5m&indicators=close&includeTimestamps=false&includePrePost=false&corsDomain=finance.yahoo.com&.tsrc=finance"
-
-
-
-
+#
+# def set_ticker_and_url(ticker):
+#     ticker = ticker
+#     url = "https://query1.finance.yahoo.com/v7/finance/quote?symbols="+ ticker + "&range=1d&interval=5m&indicators=close&includeTimestamps=false&includePrePost=false&corsDomain=finance.yahoo.com&.tsrc=finance"
+#
+#
+#
 
 ## this url puts the ticker variable into the yahoo query1 api
-url = "https://query1.finance.yahoo.com/v7/finance/quote?symbols="+ ticker + "&range=1d&interval=5m&indicators=close&includeTimestamps=false&includePrePost=false&corsDomain=finance.yahoo.com&.tsrc=finance"
-
-# print(url)
-
 def stdout(price_data, market_time,stock_name):
     try:
-        f = open("./../../data/"+stock_name+'_'+str(date.today())+ ".txt", 'a')
+        f = open("./../../data/"+stock_name+'_'+str(date.today())+ ".csv", 'a')
     except FileNotFoundError:
-        f = open("./../../data/"+stock_name+'_'+str(date.today())+ ".txt", 'w')
+        f = open("./../../data/"+stock_name+'_'+str(date.today())+ ".csv", 'w')
     finally:
         f.write(str(str(market_time) + ',' + str(price_data)))
         f.write('\n')
@@ -56,11 +50,10 @@ def periodic_fetch_loop():
         ## try fetching every 10 seconds
         get_new_data()
         time.sleep(9)
-        print("Getting new data")
-
 
 
 if __name__ == '__main__':
     ticker = input('Enter the symbol for the stock you want to track. A full list can be found on the yahoo finance website \n \n')
     url = "https://query1.finance.yahoo.com/v7/finance/quote?symbols="+ ticker + "&range=1d&interval=5m&indicators=close&includeTimestamps=false&includePrePost=false&corsDomain=finance.yahoo.com&.tsrc=finance"
+    print("Pass this as an argument to the plotting module for it to start up"+ "./../../data/"+ticker+'_'+str(date.today())+ ".csv")
     periodic_fetch_loop()
