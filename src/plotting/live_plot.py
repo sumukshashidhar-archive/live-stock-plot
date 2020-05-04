62dc59f5192f5fe62c7eb6e09f31e962720384e9
sumukshashidhar <sumukuuu@gmail.com>
2020-05-04 09:40:05 +0530

    added an interval argument and finished testing

--- src/plotting/live_plot.py
+++ src/plotting/live_plot.py
@@ -3,21 +3,27 @@ import matplotlib.animation as animation
 from matplotlib import style
 import argparse
 
+# styling
+style.use('seaborn-darkgrid')
+
+#parsing arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--path", help="Add the filepath as a string")
+parser.add_argument("-i", "--interval", help="Time in seconds before updates to the graph")
 args = parser.parse_args()
+inter = args.interval if args.interval!=None else 5
 
 if args.path == None:
     print("Please pass in the path argument for this to work")
     exit()
 
 
-style.use('seaborn-darkgrid')
-
 file = args.path
 fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
 
+
+# main animation function
 def animate(i):
     graph_data = open(file,'r').read()
     lines = graph_data.split('\n')
@@ -32,5 +38,5 @@ def animate(i):
     ax1.plot(xs, ys)
 
 
-ani = animation.FuncAnimation(fig, animate, interval=5000)
+ani = animation.FuncAnimation(fig, animate, interval=inter)
 plt.show()
