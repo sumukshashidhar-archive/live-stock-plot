5da3a2bb066c18ce76c16556fd9968575331a0ed
sumukshashidhar <sumukuuu@gmail.com>
2020-05-04 10:28:08 +0530

    added a realtime module and started writing seconds elapsed instead of unix time

--- src/retrieve/realtime.py
+++ src/retrieve/realtime.py
@@ -10,26 +10,6 @@ from datetime import date
 import time
 import argparse
 
-
-
-def clean(history, filename):
-	"""
-	This function takes in the max allowed history and deletes old lines
-
-	Arguments:
-	history {int} -- The max allowed lines
-	filename {String} -- The path to the file you need to clean
-	"""
-
-	with open(filename, "r") as fclean:
-		lines = fclean.readlines()
-
-	if len(lines) >= history:
-		with open(filename, "w") as fcleaned:
-			for i in range(1, len(lines)):
-				fcleaned.write(lines[i])
-
-
 ## this url puts the ticker variable into the yahoo query1 api
 def stdout(price_data, market_time,stock_name):
 	"""
@@ -48,8 +28,7 @@ def stdout(price_data, market_time,stock_name):
 		except:
 			print("You are running the file directly from the source directory. \n This is not supported at the time \n  Please run it from the root directory of the repo")
 	finally:
-		clean(history, str("./data/"+stock_name+'_'+str(date.today())+".csv"))
-		f.write(str(str(market_time - t_start) + ',' + str(price_data)))
+		f.write(str(str(market_time) + ',' + str(price_data)))
 		f.write('\n')
 		f.close()
 
@@ -65,7 +44,6 @@ def data_parse(new_data):
     string = new_data.read().decode('utf-8')
     data_parsed = json.loads(string)
     data_parsed = data_parsed["quoteResponse"]["result"][0]
-    # print(json.dumps(data_parsed, indent=4)) ## for debugging purposes
     stdout(data_parsed["regularMarketPrice"], data_parsed["regularMarketTime"], ticker)
 
 def get_new_data():
@@ -76,40 +54,30 @@ def get_new_data():
     data_parse(new_data)
 
 
-def periodic_fetch_loop(seconds):
+def periodic_fetch_loop():
     """
     Just to keep collecting data
     """
     while(True):
         ## try fetching every 10 seconds
         get_new_data()
-        time.sleep(seconds)
 
 
 
 
 # Parsing arguments passed from the command line
 parser = argparse.ArgumentParser()
-parser.add_argument("-r", "--recall", help="Set the max history for the plot. DEFAULT=200")
 parser.add_argument("-t", "--ticker", help="Add the ticker")
-parser.add_argument("-s", "--seconds", help="The number of seconds between each fetch. DEFAULT=10")
 args = parser.parse_args()
 
 
 # setting defaults if arguments weren't passed
-history = int(args.recall) if args.recall!=None else 200
-seconds = int(args.seconds) if args.seconds!=None else 10
 ticker = args.ticker if args.ticker!=None else "^BSESN"
-
-
-
-# start time.
-t_start = time.time()
 
 url = "https://query1.finance.yahoo.com/v7/finance/quote?symbols="+ ticker + "&range=1d&interval=5m&indicators=close&includeTimestamps=false&includePrePost=false&corsDomain=finance.yahoo.com&.tsrc=finance"
 print("\n\n\n\n")
-print(f'Getting data for: {ticker} with an interval of {seconds} and showing data upto {history} iterations')
+print(f'Getting data for: {ticker} realtime')
 print("\n\n\n\n")
 print("Pass this as an argument to the plotting module for it to start up: "+ "./data/"+ticker+'_'+str(date.today())+ ".csv" + "\n\n")
 print("Killing this terminal stops data retrieval. Do so at your own risk")
-periodic_fetch_loop(seconds)
+periodic_fetch_loop()
